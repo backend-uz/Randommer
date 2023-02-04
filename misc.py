82e64c97f6291ab4303d50415bd6f9d18616fce8
user1 <backenduz@gmail.com>
2023-02-04 16:05:37 +0500

    misc

--- misc.py
+++ misc.py
@@ -12,7 +12,9 @@ class Misc(Randommer):
         Returns:
             list: list of cultures
         '''
-        pass
+        headers = {"X-Api-Key":api_key}
+        r = requests.get(f'{self.get_url()}Misc/Cultures', headers=headers)
+        return r.json()
     
     def get_random_address(self, api_key: str, number: int, culture='en') -> list:
         '''get available misc cultures
@@ -25,4 +27,11 @@ class Misc(Randommer):
         Returns:
             list: random address
         '''
-        pass
+        headers = {"X-Api-Key":api_key}
+        params = {"number": number, "culture": "en"}
+        r = requests.get(f'{self.get_url()}Misc/Random-Address', params=params, headers=headers)
+        return r.json()
+
+misc = Misc()
+key = "689160d3ed5c445a8ddf7b1820957a75"
+print(misc.get_random_address(key, 3))
