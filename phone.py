4289f76f0e2cf14aed837952194211a09ff536fa
user1 <backenduz@gmail.com>
2023-02-04 22:50:26 +0500

    phone

--- phone.py
+++ phone.py
@@ -14,7 +14,11 @@ class Phone(Randommer):
         Returns:
             list: list of phone numbers
         '''
-        pass
+        headers = {"X-Api-Key":api_key}
+        params = {"CountryCode": CountryCode, "Quantity":Quantity}
+        r = requests.get(f'{self.get_url()}Phone/Generate', params=params, headers=headers)
+        return r.json()
+    
     
     def get_IMEI(self, api_key: str, Quantity: int) -> list:
         '''get bulk imei
@@ -26,20 +30,24 @@ class Phone(Randommer):
         Returns:
             list: list of phone numbers
         '''
-        pass
+        headers = {"X-Api-Key":api_key}
+        params = {"Quantity": Quantity}
+        r = requests.get(f'{self.get_url()}Phone/IMEI', params=params, headers=headers)
+        return r.json()
     
     def is_valid(self, api_key: str, telephone: str, CountryCode: str) -> bool:
         '''get bulk imei
-
         Args:
             api_key (str): api key
             telephone (str): phone number
             CountryCode (str): CountryCode ex: 'uz'
-
         Returns:
             bool: is valid
         '''
-        pass
+        headers = {"X-Api-Key": api_key}
+        payload = {"telephone": telephone, "CountryCode": CountryCode}
+        r = requests.get(f'{self.get_url()}Phone/Validate', params=payload, headers=headers)
+        return r.json()
     
     def get_countries(self, api_key: str) -> list:
         '''get countries
@@ -50,4 +58,10 @@ class Phone(Randommer):
         Returns:
             list: lsit of countries
         '''
-        pass
+        headers = {"X-Api-Key": api_key}
+        r = requests.get(f'{self.get_url()}Phone/Countries', headers=headers)
+        return r.json()
+
+phone = Phone()
+key = "689160d3ed5c445a8ddf7b1820957a75"
+print(phone.get_countries(key))
